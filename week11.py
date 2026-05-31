a83ea29ac86f5ff25b25472c0dbbfc6f3afe8ac8
RachelWantuch <80309987+RachelWantuch@users.noreply.github.com>
2026-05-31 11:21:05 -0700

    change title

--- week11.py
+++ week11.py
@@ -4,7 +4,7 @@ from pyspark.ml.regression import LinearRegression
 import happybase
 
 # Step 1: Create a Spark session
-spark = SparkSession.builder.appName("MLib Student Sucess Prediction").enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.appName("MLib Athlete Success").enableHiveSupport().getOrCreate()
 
 # Step 2: Load the data from the Hive table 'gradesml' into a Spark DataFrame
 athlete_df = spark.sql("SELECT athlete_id, age, gender, height_cm, weight_kg, training_intensity, training_hours_per_week, recovery_days_per_week,  match_count_per_week, rest_between_event_days, fatigue_score, performance_score, team_contribution_score, load_balance_score, acl_risk_score, injur_indicator, heartbeat FROM athlete")
